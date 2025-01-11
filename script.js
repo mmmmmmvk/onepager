9d6e7d348749fabaecc725b95714d628d1cbafa4
mmmmmmvk <maximilianvdovkin@gmail.com>
2025-01-11 21:28:19 +0100

    added scroll-triggered animations

---
+++ script.js
@@ -0,0 +1,19 @@
+const observerSettings = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add ('visible');
+        } else {
+            entry.target.classList.remove ('visible');
+        }
+    });
+}, observerSettings);
+
+document.querySelectorAll('.animate-element').forEach((element) => {
+    observer.observe(element);
+});
